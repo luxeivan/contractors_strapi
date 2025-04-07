8d1b82099d6a35df1c478862114634ed1df3a728
luxeivan <luxeivan@gmail.com>
2025-04-07 16:10:05 +0300

    доработки

--- src/api/contractor/controllers/contractor.js
+++ src/api/contractor/controllers/contractor.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 /**
  * contractor controller
  */
@@ -8,9 +9,22 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::contractor.contractor', ({ strapi }) => ({
     // Method 1: Creating an entirely custom action
-    async myContractors(ctx) {
+    
+    async find(ctx) {
+        let sanitizedQueryParams = await this.sanitizeQuery(ctx);
+        const { id } = ctx.state.user;
+        sanitizedQueryParams.filters = {
+            ...sanitizedQueryParams.filters, user: {
+                id: {
+                    $eq: id || null
+                }
+            }
+        }
+        const entity = await strapi.service('api::contractor.contractor').find(sanitizedQueryParams)        
+        const sanitizeResult = await this.sanitizeOutput(entity)
+
         try {
-            ctx.body = '123123';
+            ctx.body = sanitizeResult;
         } catch (err) {
             ctx.body = err;
         }
